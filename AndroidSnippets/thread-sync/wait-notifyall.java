fe31179688d94e730ebc73b285a7946c76d70c77
ihla <lubos.ilcik@gmail.com>
2013-06-19 10:27:40 +0200

    - wait-notifyall

---
+++ AndroidSnippets/thread-sync/wait-notifyall.java
@@ -0,0 +1,46 @@
+import com.badlogic.androidgames.framework.impl.GLGame.GLGameState;
+
+//init
+enum State {
+        Initialized,
+        Running,
+        Paused,
+        Finished,
+        Idle
+    }
+
+State state = State.Initialized;
+Object stateChanged = new Object(); //lock object
+
+//calling from UI thread
+@Override 
+public void onPause() {        
+    synchronized(stateChanged) { // lock???
+        state = State.Paused;
+        while(true) {
+            try {
+                stateChanged.wait(); // wait for notifyall
+                break;
+            } catch(InterruptedException e) { 
+            }
+        }
+    }
+    super.onPause();
+}
+
+//calling from loop in helper thread
+//both UI thread and helper thread share the state and lock objects
+State state = null;
+
+synchronized(stateChanged) {
+    state = this.state;
+}
+
+if(state == State.Paused) {
+    // ...do onPause processing            
+    synchronized(stateChanged) {
+        this.state = State.Idle;
+        stateChanged.notifyAll(); // notify UI thread on processing finished
+    }
+}
+
