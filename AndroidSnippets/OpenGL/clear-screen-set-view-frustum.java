2b3e00acf9f4d875b1bb44ea7b11c889ebca6532
ihla <lubos.ilcik@gmail.com>
2013-06-19 14:53:36 +0200

    - comment

--- AndroidSnippets/OpenGL/clear-screen-set-view-frustum.java
+++ AndroidSnippets/OpenGL/clear-screen-set-view-frustum.java
@@ -3,7 +3,10 @@ import javax.microedition.khronos.opengles.GL10;
 // clear screen
 gl.glClearColor(0,0,0,1);
 gl.glClear(GL10.GL_COLOR_BUFFER_BIT); // set background color to black
-// set view port
+/* set view port
+ * OpenGL ES uses the viewport as a way to translate the coordinates of points projected to
+ * the near clipping plane to framebuffer pixel coordinates.
+ */
 gl.glViewport(0, 0, getViewWidth(), getViewHeight()); // set view port to whole screen size
 // set orthogonal projection
 gl.glMatrixMode(GL10.GL_PROJECTION);
