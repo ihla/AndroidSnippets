b743f03abd24a10cd9d39adeaab703f2ea8b12d9
ihla <lubos.ilcik@gmail.com>
2013-06-20 09:14:37 +0200

    - comment, translation of coordinates

--- AndroidSnippets/OpenGL/setting-view-frustum-with-zoom.java
+++ AndroidSnippets/OpenGL/setting-view-frustum-with-zoom.java
@@ -1,5 +1,6 @@
 /*
  * view port is centered around (x,y)
+ * x and y are our camera’s position in world space
  * zoom < 1 - zooming in
  * zoom > 1 - zooming out
  */
@@ -7,3 +8,9 @@ gl.glOrthof(x – FRUSTUM_WIDTH / 2 * zoom,
 		x + FRUSTUM_WIDTH / 2 * zoom, 
 		y – FRUSTUM_HEIGHT / 2 * zoom, 
 		y + FRUSTUM_HEIGHT / 2 * zoom, 1, -1);
+
+/*
+ * translation of touch coordinates into our world coordinate
+ */
+worldX = (touchX / Graphics.getWidth()) × FRUSTUM_WIDTH + x – FRUSTUM_WIDTH / 2;
+worldY = (1 – touchY / Graphics.getHeight()) × FRUSTUM_HEIGHT + y – FRUSTUM_HEIGHT / 2;
